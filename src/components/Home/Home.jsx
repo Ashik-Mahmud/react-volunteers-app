a2a4e4623255873ceead499102b144699937dc28
ashikmahmud <ashikmamud187@gmail.com>
2022-04-06 00:41:02 +0600

    yeah ready do to the functionality now done my design part

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,69 @@
 import React from "react";
+import styled from "styled-components";
+import Volunteer from "../Volunteer/Volunteer";
 
 const Home = () => {
   return (
     <section id="home">
       <div className="container">
-        <h1>Home</h1>
+        <SectionTitle>
+          <div>
+            <h1>Volunteers</h1>
+            <p>charity is most unique virtue in the world.</p>
+          </div>
+          <div className="search">
+            <input type="search" placeholder="Search Volunteers" />
+            <button>Search</button>
+          </div>
+        </SectionTitle>
+        <div className="volunteer-container">
+          <Volunteer />
+          <Volunteer />
+          <Volunteer />
+          <Volunteer />
+        </div>
       </div>
     </section>
   );
 };
+
+const SectionTitle = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #f8f8f8;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  margin: 1rem 0rem;
+  .search {
+    display: flex;
+    align-items: stretch;
+    position: relative;
+    background-color: var(--white-color);
+    padding: 0.3rem;
+    border-radius: 50px;
+    width: min(100% - 2rem, 400px);
+    input {
+      background-color: transparent;
+      border: none;
+      outline: none;
+      font-size: 1rem;
+      font-family: "poppins";
+      width: 100%;
+      padding: 0.6rem 1rem;
+    }
+    button {
+      padding: 0rem 1.2rem;
+      background-color: var(--primary-color);
+      color: var(--white-color);
+      font-size: 1rem;
+      border: none;
+      outline: none;
+      font-family: "poppins";
+      border-radius: 50px;
+    }
+  }
+`;
 
 export default Home;
---
+++ src/components/Volunteer/Volunteer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+
+const Volunteer = () => {
+  return (
+    <VolunteerContainer>
+      <div className="image">
+        <img src="https://imgur.com/Q3D72V1.png" alt="child" />
+      </div>
+      <div className="info">
+        <h3>Child Abuse</h3>
+        <button>Donate</button>
+      </div>
+    </VolunteerContainer>
+  );
+};
+const VolunteerContainer = styled.div`
+  position: relative;
+  box-shadow: 0px 0px 13px rgba(0, 0, 0, 0.08);
+  padding: 0.5rem;
+  border-radius: 5px;
+  .image {
+    height: 240px;
+    overflow: hidden;
+    border-radius: 5px;
+    img {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+  .info {
+    margin: 0.6rem 0rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    button {
+      padding: 0.2rem 0.6rem;
+      border: none;
+      outline: none;
+      background-color: var(--primary-color);
+      color: var(--white-color);
+      font-family: "poppins";
+      font-size: 0.9rem;
+      border-radius: 5px;
+    }
+  }
+  h3 {
+    color: var(--primary-color);
+  }
+`;
+export default Volunteer;
