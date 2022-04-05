0fbd66f234cbe25e0ec912d79fdda11e3b64b187
ashikmahmud <ashikmamud187@gmail.com>
2022-04-05 06:30:31 +0600

    initlization react-volunteers application

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <section id="header">
+      <div className="container">
+        <h1>Header</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
---
+++ src/components/Volunteers/Volunteers.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Volunteers = () => {
+  return (
+    <section id="volunteers">
+      <div className="container">
+        <h1>Volunteers</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Volunteers;
